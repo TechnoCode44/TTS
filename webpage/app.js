668235e0a3a66acfd7ee950206603f86c88bff7b
TechnoCode44 <ElmoSmiley@outlook.com>
2023-06-14 21:10:26 +0100

    Post request is sent with text on submit

--- webpage/app.js
+++ webpage/app.js
@@ -1,1 +1,19 @@
-console.log("Hi!")
+function submit() {
+    const text = document.getElementById("text");
+
+    const json = JSON.stringify(
+        {
+            "text": text.value
+        }
+    );
+    fetch("/tts", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          },
+        body: json
+    });
+
+    text.value = ""
+}
